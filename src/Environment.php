e376c3fecb563df84804f26d420f9a4edad6616e
clvarley <conorlukevarley@gmail.com>
2021-10-11 17:33:58 +0100

    Added methods to check dev & debug status

--- src/Environment.php
+++ src/Environment.php
@@ -4,6 +4,8 @@ namespace Swiftly\TwigBridge;
 
 use Swiftly\Config\Store;
 
+use function in_array;
+
 /**
  * Utility used to manage Twig and environment configuration
  *
@@ -50,5 +52,30 @@ Class Environment
     public function setTemplateDir( string $template_dir ) : void
     {
         $this->template_dir = $template_dir;
+    }
+
+    /**
+     * Determine whether we are currently running in dev mode
+     *
+     * @return bool Is development?
+     */
+    public function isDev() : bool
+    {
+        return in_array( $this->config->get( 'core.environment' ),
+            ['dev', 'development'],
+            true
+        );
+    }
+
+    /**
+     * Does the user wish to see debug messages
+     *
+     * @return bool Is debug?
+     */
+    public function isDebug() : bool
+    {
+        return ( $this->isDev()
+            && $this->config->get( 'core.warnings', false )
+        );
     }
 }
