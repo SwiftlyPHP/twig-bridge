aacc0ed13ed130cfd32727400678323ce6b26bdd
clvarley <conorlukevarley@gmail.com>
2021-10-11 13:45:22 +0100

    Stubbed out primary class

---
+++ src/TwigBridge.php
@@ -0,0 +1,21 @@
+<?php
+
+use Swiftly\TwigBridge;
+use Swiftly\Config\Store;
+use Swiftly\Template\TemplateInterface;
+
+Class TwigBridge Implements TemplateInterface
+{
+
+    public function __construct()
+    {
+        // TODO
+    }
+
+    public function render( string $template, array $data = [] ) : string
+    {
+        // TODO
+
+        return '';
+    }
+}
